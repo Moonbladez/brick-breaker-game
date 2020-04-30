ac84026ba9fc9b534d481f42d4ddd54b923ed187
Bekki Young <moonbladez@hotmail.com>
2020-04-30 22:44:58 +0200

    collisions added

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ const ctx = canvas.getContext("2d")
 
 let score = 0;
 
+const brickRowCount = 9;
+const brickColCount = 5;
+
 //ball
 const ball = {
     //xposition
@@ -30,6 +33,32 @@ const paddle = {
     speed: 8,
     dx: 0
 }
+
+//create bricks
+const brickInformation = {
+    w: 70,
+    h: 20,
+    padding: 10,
+    offsetX: 45,
+    offsetY: 60,
+    visable: true,
+}
+
+const bricks = []
+for (let i = 0; i < brickRowCount; i++) {
+    bricks[i] = [];
+    for (let j = 0; j < brickColCount; j++) {
+        const x = i * (brickInformation.w + brickInformation.padding) + brickInformation.offsetX;
+        const y = j * (brickInformation.h + brickInformation.padding) + brickInformation.offsetY;
+        bricks[i][j] = {
+            x,
+            y,
+            ...brickInformation
+        }
+    }
+}
+
+
 
 //draw ball onto canvas 
 const drawBall = () => {
@@ -54,20 +83,127 @@ const drawScore = () => {
     ctx.font = "30px Arial";
     ctx.fillStyle = "#fefefe";
     ctx.fillText(`Score: ${score}`, canvas.width - 150, 40);
+}
 
+
+//draw bricks on canvas
+const drawBricks = () => {
+    bricks.forEach(column => {
+        column.forEach(brick => {
+            ctx.beginPath();
+            ctx.rect(brick.x, brick.y, brick.w, brick.h);
+            ctx.fillStyle = brick.visable ? "#9FA1A3" : "transparent";
+            ctx.fill();
+            ctx.closePath();
+        })
+    })
+}
+
+//move paddle on canvas
+const movePaddle = () => {
+    paddle.x += paddle.dx;
+
+    //wall detection
+    if (paddle.x + paddle.w > canvas.width) {
+        paddle.x = canvas.width - paddle.w
+    }
+
+    if (paddle.x < 0) {
+        paddle.x = 0;
+    }
+}
+
+//Move paddle   
+const moveBall = () => {
+    //apphend
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+
+    //wall detection x axis
+    if (ball.x + ball.size > canvas.width || ball.x - ball.size < 0) {
+        //reverse it the other way
+        ball.dx *= -1;
+    }
+
+    //wall collision top bottom
+    if (ball.y + ball.size > canvas.height || ball.y - ball.size < 0) {
+        ball.dy *= -1;
+    }
+
+    //paddle collision
+
+    if (
+        ball.x - ball.size > paddle.x &&
+        ball.x + ball.size < paddle.x + paddle.w &&
+        ball.y + ball.size > paddle.y
+    ) {
+        ball.dy = -ball.speed
+    }
+
+    //brick collision
+    bricks.forEach(column => {
+        column.forEach(brick => {
+            if (brick.visable) {
+                if (ball.x - ball.size > brick.x && //left brick check 
+                    ball.x + ball.size < brick.x + brick.w && //right brock side check
+                    ball.y + ball.size > brick.y && //top
+                    ball.y - ball.size < brick.y + brick.h //bottom
+                ) {
+                    //bounce it off brick
+                    ball.dy *= -1;
+                    brick.visable = false;
+                }
+            }
+        })
+    })
 
 }
 
-const drawEverything = (params) => {
+
+
+
+const drawEverything = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
     drawBall()
     drawPaddle()
     drawScore()
+    drawBricks()
 }
 
-drawEverything()
+
+//update canvas
+const update = () => {
+    movePaddle()
+    //draw elements
+    drawEverything()
+    moveBall()
+
+    requestAnimationFrame(update)
+}
+
+update()
 
 
+//keydown event
+const keyDown = (event) => {
+    if (event.key === "Right" || event.key === "ArrowRight") {
+        console.log(paddle.dx)
+        paddle.dx = paddle.speed;
+    } else if (event.key === "Left" || event.key === "ArrowLeft") {
+        paddle.dx = -paddle.speed;
+    }
+}
 
+// keyup
+const keyUp = (event) => {
+    if (event.key === "Right" || event.key === "ArrowRight" || event.key === "Left" || event.key === "ArrowLeft") {
+        paddle.dx = 0;
+    }
+}
+
+//keyboard event handlers
+document.addEventListener("keydown", keyDown)
+document.addEventListener("keyup", keyUp)
 
 
 
